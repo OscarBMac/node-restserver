68053819749dcd920be2367b6a08ba2fb917b7a0
Your Name <you@example.com>
2018-10-08 10:46:06 -0500

    Ariable de entorno

--- server/config/config.js
+++ server/config/config.js
@@ -12,6 +12,6 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb://cafe-user:123A56@ds125673.mlab.com:25673/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 process.env.URLDB = urlDB;
