bcb0b3d48e425a14fd5aa66cc7cfb5bff8fda0bd
Your Name <you@example.com>
2018-10-10 13:20:47 -0500

    Google Singn-In

--- server/config/config.js
+++ server/config/config.js
@@ -27,3 +27,10 @@ if (process.env.NODE_ENV === 'dev') {
     urlDB = process.env.MONGO_URI;
 }
 process.env.URLDB = urlDB;
+
+
+
+//=====================================
+//GOOGLE
+
+process.env.CLIENT_ID = process.env.CLIENT_ID || '584282521735-5v5f69dmefinjm0jc6cqfjjeq1qgfgon.apps.googleusercontent.com';
--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ require('./config/config');
 
 const express = require('express')
 const mongoose = require('mongoose');
+const path = require('path');
 
 const app = express();
 
@@ -10,7 +11,12 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use(bodyParser.json())
-    //Configuracion global de rutas
+
+//Habilitar la carpeta public
+
+app.use(express.static(path.resolve(__dirname, '../public')));
+
+//Configuracion global de rutas
 app.use(require('./routes/index'));
 
 mongoose.connect(process.env.URLDB, (err, rest) => {
